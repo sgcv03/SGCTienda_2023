171ecd6cc853b48c45c9c6d8a1826cf9ade644ba
p-sgarcia <p-sgarcia@P-SGARCIA.neoris.cxnetworks.net>
2023-04-19 13:50:19 +0200

    Commit Final

--- SGCTienda_2023/src/test/java/rf/com/tienda/dominio/TestUsuario.java
+++ SGCTienda_2023/src/test/java/rf/com/tienda/dominio/TestUsuario.java
@@ -1,5 +1,45 @@
 package rf.com.tienda.dominio;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import rf.com.tienda.exception.DomainException;
+import rf.com.tienda.util.Validator;
+
 public class TestUsuario {
+	
+
+	private static Usuario usuario;
+	
+	@BeforeAll
+	static void inicio() {
+		usuario=new Usuario();
+		
+	}
+	
+	@Test
+	void testUserNombre() throws DomainException{
+		//Caso 1: Nombre de usuario correcto
+		usuario.setUser_nombre("UsuarioCorrecto");
+		assertEquals("UsuarioCorrecto", usuario.getUser_nombre());
+		
+		//Caso 2: Nombre de usuario con menos de 5 letras
+		Assertions.assertThrows(DomainException.class,()-> {
+			usuario.setUser_nombre("MAL");
+		});
+		
+		//Caso 3: Nombre de usuario con mas de 100 letras
+		Assertions.assertThrows(DomainException.class,()-> {
+			usuario.setUser_nombre("ERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERROR");
+		});
+	}
+	
+
+	
 
 }
